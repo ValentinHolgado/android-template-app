009bc7496a9c21f9a5842784343275f7bb529f02
Valentin Holgado <val.holgado@gmail.com>
2018-03-19 18:10:46 -0300

    Refactored FilesRepository.

--- app/src/main/kotlin/ar/valentinholgado/template/backend/files/FilesRepository.kt
+++ app/src/main/kotlin/ar/valentinholgado/template/backend/files/FilesRepository.kt
@@ -6,41 +6,42 @@ import android.os.FileObserver
 import ar.valentinholgado.template.backend.BaseRepository
 import ar.valentinholgado.template.backend.Result
 import io.reactivex.Observable
+import java.io.File
 
 class FilesRepository(val context: Context) : BaseRepository<FilesAction, Result>() {
 
-    init {
-        val actionsToResults = { actions: Observable<FilesAction> ->
-            actions.map { _ ->
-                        FilesResult(
-                                status = Result.Status.SUCCESS,
-                                fileList = context
-                                        // TODO Remove Hardcoded DIRECTORY_MUSIC. Choose the folder
-                                        // using the action.
-                                        .getExternalFilesDir(DIRECTORY_MUSIC)
-                                        .listFiles()
-                                        .toList()
-                        )
-                    }
-
+    private val fileObserver = object : FileObserver(context.getExternalFilesDir(DIRECTORY_MUSIC).absolutePath) {
+        override fun onEvent(event: Int, path: String?) {
+            if (event == CREATE) outputStream.onNext(FilesResult(
+                    status = Result.Status.SUCCESS,
+                    fileList = getFiles()))
         }
+    }
 
+    private val actionsToResults = { actions: Observable<FilesAction> ->
+        actions.map { _ ->
+            FilesResult(
+                    status = Result.Status.SUCCESS,
+                    fileList = getFiles()
+            )
+        }
+    }
+
+    init {
         inputStream
                 .compose(actionsToResults)
                 .cache()
                 .subscribe(outputStream)
 
-        object : FileObserver(context.getExternalFilesDir(DIRECTORY_MUSIC).absolutePath) {
-            override fun onEvent(event: Int, path: String?) {
-                if (event == CREATE) outputStream.onNext(FilesResult(
-                        status = Result.Status.SUCCESS,
-                        fileList = context
-                                // TODO Remove Hardcoded DIRECTORY_MUSIC. Choose the folder
-                                // using the action.
-                                .getExternalFilesDir(DIRECTORY_MUSIC)
-                                .listFiles()
-                                .toList()))
-            }
-        }
+        fileObserver.startWatching()
+    }
+
+    private fun getFiles(): List<File> {
+        return context
+                // TODO Remove Hardcoded DIRECTORY_MUSIC. Choose the folder
+                // using the action.
+                .getExternalFilesDir(DIRECTORY_MUSIC)
+                .listFiles()
+                .toList()
     }
 }
